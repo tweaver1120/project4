aa7aa248712e972eb1917344c58ea1b7f6ad667c
tweaver1120 <34726827+tweaver1120@users.noreply.github.com>
2018-12-04 16:09:31 -0600

    Added bottom panel buttons

--- src/GUI/MesonetFrame.java
+++ src/GUI/MesonetFrame.java
@@ -1,6 +1,9 @@
 package GUI;
 import java.awt.BorderLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class MesonetFrame extends JFrame
@@ -8,6 +11,7 @@ public class MesonetFrame extends JFrame
     private ParameterPanel paramPanel;
     private StatisticsPanel statisticsPanel;
     private TablePanel tablePanel;
+    private BottomPanel bottomPanel;
     
     public MesonetFrame()
     {
@@ -17,14 +21,31 @@ public class MesonetFrame extends JFrame
         paramPanel = new ParameterPanel();
         statisticsPanel = new StatisticsPanel();
         tablePanel = new TablePanel();
+        bottomPanel = new BottomPanel();
         
         add(paramPanel, BorderLayout.WEST);
         add(statisticsPanel, BorderLayout.EAST);
         add(tablePanel, BorderLayout.CENTER);
+        add(bottomPanel, BorderLayout.SOUTH);
         
         setSize(1000, 450);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
         setVisible(true);
+    }
+    
+    class BottomPanel extends JPanel
+    {
+        private JButton calculate;
+        private JButton exit;
+        
+        public BottomPanel()
+        {
+            calculate = new JButton("Calculate");
+            exit = new JButton("Exit");
+            
+            add(calculate);
+            add(exit);
+        }
     }
 }
