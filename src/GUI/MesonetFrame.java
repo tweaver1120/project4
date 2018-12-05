7904b17e01d77579a818bf3b9d184fe81ef37cd0
tweaver1120 <34726827+tweaver1120@users.noreply.github.com>
2018-12-04 21:35:15 -0600

    Added File Menu Bar

--- src/GUI/MesonetFrame.java
+++ src/GUI/MesonetFrame.java
@@ -3,6 +3,8 @@ import java.awt.BorderLayout;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
@@ -12,6 +14,7 @@ public class MesonetFrame extends JFrame
     private StatisticsPanel statisticsPanel;
     private TablePanel tablePanel;
     private BottomPanel bottomPanel;
+    private TopBarFileMenu fileMenu;
     
     public MesonetFrame()
     {
@@ -22,11 +25,13 @@ public class MesonetFrame extends JFrame
         statisticsPanel = new StatisticsPanel();
         tablePanel = new TablePanel();
         bottomPanel = new BottomPanel();
+        fileMenu = new TopBarFileMenu();
         
         add(paramPanel, BorderLayout.WEST);
         add(statisticsPanel, BorderLayout.EAST);
         add(tablePanel, BorderLayout.CENTER);
         add(bottomPanel, BorderLayout.SOUTH);
+        add(fileMenu, BorderLayout.NORTH);
         
         setSize(1000, 450);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -46,6 +51,24 @@ public class MesonetFrame extends JFrame
             
             add(calculate);
             add(exit);
+        }
+    }
+    
+    class TopBarFileMenu extends JMenuBar
+    {
+        private JMenu file;
+        private JMenu open;
+        private JMenu exit;        
+        
+        public TopBarFileMenu()
+        {
+            file = new JMenu("File");
+            open = new JMenu("Open Data File");
+            exit = new JMenu("Exit");
+            
+            add(file);
+            file.add(open);
+            file.add(exit);      
         }
     }
 }
